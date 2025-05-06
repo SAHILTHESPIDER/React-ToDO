6443fbab430bf9ccd123d608dd3397accbc30ad0
SAHILTHESPIDER <sahil9594@gmail.com>
2025-05-07 00:55:06 +0530

    Updated files or made changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,28 @@ function App() {
 
   return (
     <>
-      <p className='text-[5rem]'> Hello</p>
+     <div className='w-full h-screen'>
+      <div className='flex justify-center items-center   '>
+        <div className='flex flex-col border p-4 rounded-lg mt-2'>
+          <h1 className='text-4xl uppercase'>TODO list</h1>
+           <div className='flex mt-3 space-x-3'>
+           <input type="text" className='border rounded-md p-3 ' placeholder='Enter the items'  />
+           <input type="date" className='border p-3' />
+         
+          <button className='border p-3 rounded-md'> ADD</button>
+           </div>
+           <div className='space-y-2 mt-6 flex justify-between border p-2'>
+           <span className='text-3xl'>hello</span>
+           <span className='text-3xl btn cursor-pointer border rounded-full px-2 '>X</span>
+           </div>
+           <div className='space-y-2 mt-6 flex justify-between border p-2'>
+           <span className='text-3xl'>hello</span>
+           <span className='text-3xl btn cursor-pointer border rounded-full px-2 '>X</span>
+           </div>
+          
+        </div>
+      </div>
+      </div> 
      
     </>
   )
