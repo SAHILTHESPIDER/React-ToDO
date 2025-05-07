772632026efcd2caf6d4d9b05940711cca2d01dc
SAHILTHESPIDER <sahil9594@gmail.com>
2025-05-08 05:29:59 +0530

    Updated 3.0

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ import { FaDeleteLeft } from "react-icons/fa6";
 import { IoIosAddCircle } from "react-icons/io";
 import { RiCalendarTodoFill } from "react-icons/ri";
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todo, setTodo] = useState([]);
+  const[task,setTask]=useState('');
+  const[date,setDate]=useState('');
+
+  const addtodo=()=>{
+    if(task.trim()===''||date.trim()==='')
+    {
+      return;
+    }
+    setTodo([...Todo,{id:Date.now,Text:task ,Date:date}])
+    setTask('');
+    setDate('')
+  }
+  const Deletetodo=(id)=>{
+     setTodo(Todo.filter((Todo)=>Todo.id !==id));
+  }
 
   return (
     <>
@@ -17,18 +32,26 @@ function App() {
           </div>
           
            <div className='flex mt-3 space-x-3'>
-           <input type="text" className='border rounded-md p-3  border-blue-500 px-10  ' placeholder='Things Need To Be Done'  />
-           <input type="date" className='border p-3 border-blue-500 rounded-md' />
+           <input type="text" className='border rounded-md p-3 
+            border-blue-500 px-10  'value={task} onChange={(e)=>setTask(e.target.value)} placeholder='Things Need To Be Done'  />
+           <input type="date" className='border p-3 border-blue-500 rounded-md'
+           value={date} onChange={(e)=>setDate(e.target.value)}
+           />
          
-          <button className=' p-3 rounded-md cursor-pointer border text-2xl bg-blue-200 border-blue-200 text-amber-50'><IoIosAddCircle /></button>
+          <button className=' p-3 rounded-md cursor-pointer border text-2xl bg-blue-200 border-blue-200 text-amber-50 transition hover:transform-3d'
+            onClick={addtodo} 
+          ><IoIosAddCircle /></button>
            </div>
-           <div className='space-y-2 mt-6 flex justify-between border p-2'>
-           <span className='text-[1.2rem] font-serif mt-1'>hello</span>
-           <div className='flex justify-end-safe space-x-2'>
-           <span className='text-[1.2rem] font-serif mt-1 '>22/05/2025</span>
-           <span className='text-[1.2rem]  btn cursor-pointer mt-2  px-2  '><FaDeleteLeft /></span>
+           {Todo.map((Todo)=>(
+              <div className='space-y-2 mt-6 flex justify-between border p-2'key={Todo.id}>
+           <span className='text-[1.2rem] font-serif mt-1' >{Todo.Text}</span>
+           <div className='flex justify-end-safe space-x-2'key={Todo.id}>
+           <span className='text-[1.2rem] font-serif mt-1 ' >{Todo.Date}</span>
+           <span className='text-[1.2rem]  btn cursor-pointer mt-2  px-2 '  onClick={()=>Deletetodo(Todo.id)} ><FaDeleteLeft /></span>
            </div>
            </div>
+           ))}
+           
          
           
           
